38397cf3393f8a117ebafdd53f8e38b401074994
Daniel <50109294+DanielSobieraj@users.noreply.github.com>
2020-01-06 19:31:49 +0100

    Update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/VueShop'
+        : 'https://danielsobieraj.github.io/VueShop/',
+    "transpileDependencies": [
+        "vuetify"
+    ]
+};
